c6ce16307b49e56554a6b982a4a8f1cad2c13edd
Kenneth <krbrown@live.ca>
2018-01-30 18:25:35 +0000

    With tweet character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$(".new-tweet textarea").on("keyup", function() {
+	 var count = $(this).val().length
+	var max = 140;
+	var left = max - count;
+	var button = $(this).siblings("span.counter");
+	button.text(left)
+
+	if (left < 0) {
+		document.getElementById("counter").style.color = "red"; 
+	} else {
+		document.getElementById("counter").style.color = "black"; 
+	}
+
+})
+	
+
+});
+
+
