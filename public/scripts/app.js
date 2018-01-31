2e1ac61e4188aa5514f2e53afcc51cb25a37978b
Kenneth <krbrown@live.ca>
2018-01-31 20:22:46 +0000

    fetcing tweets with Ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,60 +27,98 @@ function createTweetElement(obj){
 }
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       			"name": "Newton",
+//       			"avatars": {
+//         					"small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//         					"regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//         					"large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//       						},
+//       			"handle": "@SirIsaac"
+//     						},
+//     			"content": {
+//       						"text": "If I have seen further it is by standing on the shoulders of giants"
+//     						},
+//     			"created_at": 1461116232227
+//   							},
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": {
+//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+//       },
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   },
+//   {
+//     "user": {
+//       	"name": "Johann von Goethe",
+//       "avatars": {
+//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+//       },
+//       "handle": "@johann49"
+//     },
+//     "content": {
+//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+//     },
+//     "created_at": 1461113796368
+//   }
+// ];
 
 function renderTweets(tweets){
 	for (key of tweets){
 		var $tweet = createTweetElement(key);
-		$('#posts').append($tweet);
+		$('#posts').prepend($tweet);
 	}
 }
- renderTweets(data);
+
+
+$(".new-tweet form").on("submit", function(event) {
+  event.preventDefault()
+  console.log('Button clicked, performing ajax call...')
+	$.ajax({
+		url: 	'/tweets',
+		method: "POST",
+		data: $(this).serialize(), 
+    success: function(data) {
+      console.log('Success:', data);
+    }
+
+	})
+	
+});
+
+function loadTweets(){
+  var $button = $('#submit');
+  $button.on('click', function(){
+    console.log("button clicked");
+    $.ajax({
+      dataType: "json",
+      url: "/tweets",
+      method: "GET",
+      success: function(object){
+        renderTweets(object);
+      }
+    });
+  })
+}
+ 
+loadTweets();
+
+// event.preventDefault()
+
+
+
+ // renderTweets(data);
 	
 });
 
